4e6f879276a3c6689789aec936de68602d38d8f3
Michael Wender <michael@michaelwender.com>
2019-10-24 12:05:41 -0400

    Adding `uber_log()`

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 **Tags:** comments, spam  
 **Requires at least:** 4.5  
 **Tested up to:** 5.2.4  
-**Stable tag:** 0.1.0  
+**Stable tag:** 0.2.0  
 **License:** GPLv2 or later  
 **License URI:** https://www.gnu.org/licenses/gpl-2.0.html  
 
@@ -23,6 +23,9 @@ Setup:
 Install/Activate the plugin as you would any other WordPress plugin.
 
 ## Changelog ##
+
+### 0.2.0 ###
+* Adding `uber_log()` for enhanced error logging/debugging.
 
 ### 0.1.0 ###
 * Initial release.
--- elementor-developer-extras.php
+++ elementor-developer-extras.php
@@ -7,7 +7,7 @@
  * Author URI:      https://mwender.com
  * Text Domain:     elementor-developer-extras
  * Domain Path:     /languages
- * Version:         0.1.0
+ * Version:         0.2.0
  *
  * @package         ElementorDeveloperExtras
  */
@@ -15,3 +15,20 @@
 // Include required files
 require_once( 'lib/fns/enqueues.php' );
 require_once( 'lib/fns/shortcodes.php' );
+
+/**
+ * Enhanced error logging
+ *
+ * @param      string  $message  The message
+ */
+function uber_log( $message = null ){
+  static $counter = 1;
+
+  $bt = debug_backtrace();
+  $caller = array_shift( $bt );
+
+  if( 1 == $counter )
+    error_log( "\n\n" . str_repeat('-', 25 ) . ' STARTING DEBUG [' . date('h:i:sa', current_time('timestamp') ) . '] ' . str_repeat('-', 25 ) . "\n\n" );
+  error_log( $counter . '. ' . basename( $caller['file'] ) . '::' . $caller['line'] . ' ' . $message );
+  $counter++;
+}
